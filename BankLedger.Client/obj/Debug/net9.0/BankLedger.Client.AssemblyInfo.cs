a659bf197da7694ab17365b3a0c883519e896eeb
iancorbett <iancorbett1324@gmail.com>
2025-09-17 23:51:01 -0700

    successfully debugged isue of tailwind css styles not being applied

--- BankLedger.Client/obj/Debug/net9.0/BankLedger.Client.AssemblyInfo.cs
+++ BankLedger.Client/obj/Debug/net9.0/BankLedger.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankLedger.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44d55ddee011f560260aeccffa1f4716a1492086")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1e854c0b8e205688787414421af620cad36a680")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankLedger.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankLedger.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
